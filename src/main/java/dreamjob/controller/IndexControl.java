49d0ef5f19d41a6ee664e660a3239f514692d27d
noslav7 <noslav7@gmail.com>
2022-08-02 10:51:41 +0500

    1. Генератор HTML [#504838]

--- src/main/java/dreamjob/controller/IndexControl.java
+++ src/main/java/dreamjob/controller/IndexControl.java
@@ -1,11 +1,13 @@
 package dreamjob.controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexControl {
+
     @GetMapping("/index")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "index";
     }
 }
