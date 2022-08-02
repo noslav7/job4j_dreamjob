151a849b5e9d4333a5ec4a6e2ebaa0c42cab7457
noslav7 <noslav7@gmail.com>
2022-08-02 11:19:57 +0500

    1. Что такое клиент - серверное приложение. [#504837 #327949]

--- src/main/java/ru/job4j/dreamjob/Main.java
+++ src/main/java/ru/job4j/dreamjob/Main.java
@@ -1,4 +1,4 @@
-package dreamjob;
+package ru.job4j.dreamjob;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
@@ -1,4 +1,4 @@
-package dreamjob.controller;
+package ru.job4j.dreamjob.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
