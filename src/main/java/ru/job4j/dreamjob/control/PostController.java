0c880498baf0b2929ee5380277683f6ca2921899
noslav7 <noslav7@gmail.com>
2022-08-04 11:20:44 +0500

    4. Thymeleaf, Циклы. [#504841]

---
+++ src/main/java/ru/job4j/dreamjob/control/PostController.java
@@ -0,0 +1,17 @@
+package ru.job4j.dreamjob.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.dreamjob.store.PostStore;
+
+@Controller
+public class PostController {
+    private final PostStore store = PostStore.instOf();
+
+    @GetMapping("/posts")
+    public String posts(Model model) {
+        model.addAttribute("posts", store.findAll());
+        return "posts";
+    }
+}
