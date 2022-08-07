29a2ce72e233fa9bcc654f1f99042a69d48526ef
noslav7 <noslav7@gmail.com>
2022-08-07 11:52:57 +0500

    5. Список Вакансий [#504842 #329545]

--- src/main/java/ru/job4j/dreamjob/control/IndexControl.java
+++ src/main/java/ru/job4j/dreamjob/control/IndexControl.java
@@ -1,4 +1,4 @@
-package ru.job4j.dreamjob.controller;
+package ru.job4j.dreamjob.control;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
