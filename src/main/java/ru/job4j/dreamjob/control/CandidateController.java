f249a9128e5b59e25620d0277345e080ddfe29c6
noslav7 <noslav7@gmail.com>
2022-08-12 16:01:24 +0500

    5. Создания и редактирования кандидатов. [#504858]

--- src/main/java/ru/job4j/dreamjob/control/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/control/CandidateController.java
@@ -3,7 +3,10 @@ package ru.job4j.dreamjob.control;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.dreamjob.model.Candidate;
+import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.store.CandidateStore;
 
 @Controller
@@ -21,4 +24,18 @@ public class CandidateController {
         model.addAttribute("candidate", new Candidate(0, "Новый кандидат"));
         return "addCandidate";
     }
+
+    @PostMapping("/updatePost")
+    public String updateCandidate(@ModelAttribute Candidate candidate) {
+        store.updateCandidate(candidate);
+        return "redirect:/candidates";
+    }
+
+    @PostMapping("/createCandidate")
+    public String createPost(@ModelAttribute Candidate candidate) {
+        store.add(candidate);
+        return "redirect:/candidates";
+    }
+
+
 }
--- src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
+++ src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -5,11 +5,13 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CandidateStore {
 
     private static final CandidateStore INST = new CandidateStore();
 
+    private static AtomicInteger candidate = new AtomicInteger(3);
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private CandidateStore() {
@@ -24,5 +26,14 @@ public class CandidateStore {
 
     public Collection<Candidate> findAll() {
         return candidates.values();
+    }
+
+    public void add(Candidate candidate) {
+        candidate.setId(candidate.getId());
+        candidates.put(candidate.getId(), candidate);
+    }
+
+    public void updateCandidate(Candidate candidate) {
+        candidates.replace(candidate.getId(), candidate);
     }
 }
