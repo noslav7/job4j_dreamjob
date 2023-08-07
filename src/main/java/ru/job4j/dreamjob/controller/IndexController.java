e8fc922c664419d26bcbaa86b9f9b8978a3ed172
noslav7 <noslav7@gmail.com>
2023-08-07 10:50:40 +0500

    1. Генератор HTML [#504838 # [#504838 #327960]]

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -1,14 +1,14 @@
 package ru.job4j.dreamjob.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
     @GetMapping("/index")
     public String getIndex() {
-        return "Hello World!";
+        return "index";
     }
 
 }
