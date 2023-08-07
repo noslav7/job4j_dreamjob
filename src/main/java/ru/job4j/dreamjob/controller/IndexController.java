8a347efca3b92f61e56d8b0bad282312c78455dd
noslav7 <noslav7@gmail.com>
2023-08-07 10:07:28 +0500

    Реализован "Hello World!

---
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -0,0 +1,14 @@
+package ru.job4j.dreamjob.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/index")
+    public String getIndex() {
+        return "Hello World!";
+    }
+
+}
