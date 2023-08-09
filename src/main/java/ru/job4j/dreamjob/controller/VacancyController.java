3c3d5a60fcb341786ab3b52397012ea92968b2ec
noslav7 <noslav7@gmail.com>
2023-08-09 11:45:02 +0500

    8. HTML form. Страница создания вакансии [#504844 #329787]

--- src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
+++ src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
@@ -19,4 +19,8 @@ public class VacancyController {
         return "vacancies/list";
     }
 
+    @GetMapping("/create")
+    public String getCreationPage() {
+        return "vacancies/create";
+    }
 }
