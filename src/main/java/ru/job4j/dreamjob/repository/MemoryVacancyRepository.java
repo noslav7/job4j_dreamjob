55c0d6a4593a0e397e65e5358f39e076429023f6
noslav7 <noslav7@gmail.com>
2023-08-12 09:07:56 +0500

    5. Просмотр, редактирование и удаление вакансии [#504850 #331360]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -43,7 +43,8 @@ public class MemoryVacancyRepository implements VacancyRepository {
     @Override
     public boolean update(Vacancy vacancy) {
         return vacancies.computeIfPresent(vacancy.getId(), (id, oldVacancy) ->
-                new Vacancy(oldVacancy.getId(), vacancy.getTitle(), vacancy.getDescription())) != null;
+                new Vacancy(oldVacancy.getId(), vacancy.getTitle(),
+                        vacancy.getDescription())) != null;
     }
 
     @Override
